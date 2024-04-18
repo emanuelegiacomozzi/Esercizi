b04626219b190d5234273cf7d8704c8d2827fff6
emanuelegiacomozzi <emanuelegiacomozzi2018@gmail.com>
2024-04-18 13:56:18 +0200

    esercizio fino al 3.7

--- lezione2/lezione2.py
+++ lezione2/lezione2.py
@@ -79,6 +79,48 @@ print(f"Ciao {guest[0]} vuoi venire a cena?")
 print(f"Ciao {guest[1]} vuoi venire a cena?")
 print(f"Ciao {guest[2]} vuoi venire a cena?")
 
+'''
+3-5. Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+• Start with your program from Exercise 3-4. Add a print() call at the end of your program, stating the name of the guest who can’t make it.
+• Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+• Print a second set of invitation messages, one for each person who is still in your list.
+'''
+guest:list = ["Pino", "Franco", "Gianluca"]
+print(f"{guest[2]} non potrà partecipare")
+guest[2] = "Marco"
+print(f"Ciao {guest[0]} vuoi venire a cena?")
+print(f"Ciao {guest[1]} vuoi venire a cena?")
+print(f"Ciao {guest[2]} vuoi venire a cena?")
+
+'''
+3-6. More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+• Start with your program from Exercise 3-4 or 3-5. Add a print() call to the end of your program, informing people that you found a bigger table.
+• Use insert() to add one new guest to the beginning of your list.
+• Use insert() to add one new guest to the middle of your list.
+• Use append() to add one new guest to the end of your list.
+• Print a new set of invitation messages, one for each person in your list.
+'''
+guest:list = ["Pino", "Franco", "Gianluca"]
+print("Ho trovato una sala più grande")
+guest.insert(0, "Gianni")
+guest.insert(2, "Maria")
+guest.append("Sofia")
+print(f"Ciao {guest[0]} vuoi venire a cena?")
+print(f"Ciao {guest[1]} vuoi venire a cena?")
+print(f"Ciao {guest[2]} vuoi venire a cena?")
+print(f"Ciao {guest[3]} vuoi venire a cena?")
+print(f"Ciao {guest[4]} vuoi venire a cena?")
+print(f"Ciao {guest[5]} vuoi venire a cena?")
+
+'''
+3-7. Shrinking Guest List: You just found out that your new dinner table won’t arrive in time for the dinner, and now you have space for only two guests.
+• Start with your program from Exercise 3-6. Add a new line that prints a message saying that you can invite only two people for dinner.
+• Use pop() to remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from your list, print a message to that person letting them know you’re sorry you can’t invite them to dinner.
+• Print a message to each of the two people still on your list, letting them know they’re still invited.
+• Use del to remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program.
+'''
+
+
 
 
 
