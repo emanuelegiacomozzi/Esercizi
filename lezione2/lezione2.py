fd37543911936c804bcaa85cdebb9ce9b9539e0e
emanuelegiacomozzi <emanuelegiacomozzi2018@gmail.com>
2024-04-18 13:32:58 +0200

    esercizi fino al 3-4

--- lezione2/lezione2.py
+++ lezione2/lezione2.py
@@ -30,5 +30,59 @@ including the quotation marks: Albert Einstein once said,
 “A person who never made a mistake never tried anything new.”
 '''
 quote:str = "Forza Roma"
-Name:str = "Francesco Totti"
-print(f"{Name} once said {quote} ")
+famous_person:str = "Francesco Totti"
+print(f"{famous_person} once said \"{quote}\" ")
+
+'''
+2-8. File Extensions: Python has a removesuffix() method that works exactly like removeprefix(). 
+Assign the value 'python_notes.txt' to a variable called filename.
+ Then use the removesuffix() method to display the filename without the file extension, like some file browsers do.
+'''
+filename:str = "python_notes.txt"
+print(filename.removesuffix('.txt'))
+
+'''
+3-1. Names: Store the names of a few of your friends in a list called names.
+ Print each person’s name by accessing each element in the list, one at a time.
+'''
+Names = ["Giorgio", "Mario", "Simone", "Carlo"]
+for i in Names:
+    print(i)
+
+'''
+3-2. Greetings: Start with the list you used in Exercise 3-1,
+ but instead of just printing each person’s name, print a message to them. 
+The text of each message should be the same, but each message should be personalized with the person’s name.
+'''
+Names = ["Giorgio", "Mario", "Simone", "Carlo"]
+message = "come stai?"
+for i in Names:
+    print(f"{i} {message}")
+
+'''
+3-3. Your Own List: Think of your favorite mode of transportation,
+ such as a motorcycle or a car, and make a list that stores several examples. 
+ Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.
+'''
+
+mezzo_trasporto:list = ["motocicletta", "panda", "autobus"]
+print(f"Vorrei comprare una {mezzo_trasporto[0]}, ma guido una {mezzo_trasporto[1]}, perché non voglio prendere l'{mezzo_trasporto[2]}")
+
+'''
+3-4. Guest List: If you could invite anyone, living or deceased,
+ to dinner, who would you invite? Make a list that includes at 
+ least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+'''
+
+guest:list = ["Pino", "Franco", "Gianluca"]
+print(f"Ciao {guest[0]} vuoi venire a cena?")
+print(f"Ciao {guest[1]} vuoi venire a cena?")
+print(f"Ciao {guest[2]} vuoi venire a cena?")
+
+
+
+
+
+
+
+
