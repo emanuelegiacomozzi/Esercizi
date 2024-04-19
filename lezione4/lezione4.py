d5a88f866a4e782c2cf60b17c91dde6d34ed9eca
emanuelegiacomozzi <emanuelegiacomozzi.2018@gmail.com>
2024-04-19 12:54:59 +0200

    esercizio 2

--- lezione4/lezione4.py
+++ lezione4/lezione4.py
@@ -1,4 +1,19 @@
+#esercizio 1
+
 def subtract(x:float, y:float):
     sub = x - y
     return sub
 print(subtract(10,8))
+
+#esercizio 2
+
+def check_value(x:int):
+    if x > 5:
+        print(f"{x} è maggiore di 5")
+    elif x < 5:
+        print(f"{x} è minore di 5")
+    else:
+        print(f"{x} è uguale a 5")
+    return x
+x:int = int(input("Inserisci un numero: "))
+check_value(x)
