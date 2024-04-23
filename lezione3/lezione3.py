755268937fd2f18e37bcf5ae5658819dcfc12066
emanuelegiacomozzi <emanuelegiacomozzi.2018@gmail.com>
2024-04-23 19:21:07 +0200

    esercizio 5

--- lezione3/lezione3.py
+++ lezione3/lezione3.py
@@ -16,5 +16,48 @@ pizza_type(pizza)
 4-2. Animals: Think of at least three different animals that have a common characteristic. Store the names of these animals in a list, and then use a for loop to print out the name of each animal.
 • Modify your program to print a statement about each animal, such as A dog would make a great pet.
 • Add a line at the end of your program, stating what these animals have in common. You could print a sentence, such as Any of these animals would make a great pet!
+'''
+
+def animal_type(animal:list): #i use a function called animal_type that return the animals list
+    for elem in animal:
+        if elem == "Elephant":
+            print(f"The {animal[0]} has fangs") #if the animal in list is the elephant, print this message
+        elif elem == "Giraffe":
+            print(f"The {animal[1]} has a long neck") #if the animal is the giraffe, print this message
+        else:
+            print(f"The {animal[2]} lives in the seas") #else print this message
+    print("Each of these animals is very big")
+    return elem 
+animal:list = ["Elephant", "Giraffe", "Whale"]
+animal_type(animal)
 
 '''
+4-3. Counting to Twenty: Use a for loop to print the numbers from 1 to 20, inclusive.
+'''
+
+numbers:int = range(1,21)
+for n in numbers:
+    print(n)
+
+'''
+4-4. One Million: Make a list of the numbers from one to one million, and then use a for loop to print the numbers. (If the output is taking too long, stop it by pressing CTRL-C or by closing the output window.)
+'''
+n:int = 1 #i've initialized a variable n and i've assigned the value 1
+n1:int = 1000001 #i've initialized a variable n1 and i've assigned the value 1000001 because the range is from 1 to 1000000 inclusive
+numbers:list = list(range(n,n1)) #'i've make a list which has as range n(1), n1(1000000)
+print(numbers)
+
+'''
+4-5. Summing a Million: Make a list of the numbers from one to one million, and then use min() and max() to make sure your list actually starts at one and ends at one million. Also, use the sum() function to see how quickly Python can add a million numbers.
+'''
+
+
+numbers:list = list(range(1,1000001))
+min_number:int = min(numbers)
+print(min_number)
+max_number:int = max(numbers)
+print(max_number)
+sum_numbers:int = sum(numbers)
+
+
+ 
