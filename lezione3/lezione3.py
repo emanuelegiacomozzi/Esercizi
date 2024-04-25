83f13beb34ec610ffa4aeab38f9805b6e0d52aaa
emanuelegiacomozzi <emanuelegiacomozzi2018@gmail.com>
2024-04-25 19:26:48 +0200

    esercizi finiti

--- lezione3/lezione3.py
+++ lezione3/lezione3.py
@@ -176,3 +176,242 @@ print(max_number)
 sum_numbers:int = sum(numbers)
 print(sum_numbers)
  
+'''
+5-1. Conditional Tests: Write a series of conditional tests. Print a statement
+describing each test and your prediction for the results of each test. Your code
+should look something like this:
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+• Look closely at your results, and make sure you understand why each line
+evaluates to True or False.
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another
+5 tests evaluate to False.
+'''
+car:str = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+print("\nIs car != 'fiat'? I predict True.")
+print(car != 'fiat')
+car:str = 'blue'
+print(f"\nIs car == 'white'? I predict False.")
+print(car == 'white')
+print("\nIs car == 'blue'? I predict True.")
+print(car == 'blue')
+car:str = 'cheap'
+print("\nIs car == 'cheap'? I predict True.")
+print(car == 'cheap')
+print("\nIs car == 'expensive'? I predict False.")
+print(car == 'expensive')
+car:str = 'small'
+print("\nIs car == 'big'? I predict False.")
+print(car == 'big')
+print("\nIs car == 'small'? I predict True.")
+print(car == 'small')
+print("\nIs len(car) == 4? I predict False.")
+print(len(car) == 4)
+
+'''
+5-2. More Conditional Tests: You don’t have to limit the number of tests you cre-
+ate to 10. If you want to try more comparisons, write more tests and add them
+
+to conditional_tests.py. Have at least one True and one False result for each of
+the following:
+• Tests for equality and inequality with strings
+• Tests using the lower() method
+• Numerical tests involving equality and inequality, greater than and less
+than, greater than or equal to, and less than or equal to
+• Tests using the and keyword and the or keyword
+• Test whether an item is in a list
+• Test whether an item is not in a list
+'''
+name:str = 'Mario'
+print("Name == 'Mario'?I predict True")
+print(name == 'Mario')
+print("Name != 'Mario'?I predict False")
+print(name != 'Mario')
+print("\nIs name.lower() == 'Mario'.lower()? I predict True" )
+print(name.lower() == 'Mario'.lower())
+print("\nIs 10 > 9? I predict True")
+print(10 > 9)
+print("Is 8 > 9? I predict False")
+print(8 > 9)
+print("Is 10 >= 11? I predict False")
+print(10 >= 11)
+print("Is 10 <= 10? I predict True")
+print(10 <= 10)
+print("Is 11 == 11? I predict True")
+print(11 == 11)
+print("Is 10 != 10? I predict False")
+print(10 != 10)
+print("\nIs 10 > 9 and 10 > 11? I predict False")
+print(10 > 9 and 10 > 11)
+print("Is 10 > 9 and 10 > 11? I predict True")
+print(10 > 9 or 10 > 11)
+color:list = ["Blue", "Black", "Red", "Brown"]
+print("\nIs Red in this list? I predict True")
+print("Red" in color)
+print("Is black in this list? I predict False")
+print("black" in color)
+
+'''
+5-3. Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+• Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+• Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+'''
+alien_color:str = "Green"
+if alien_color == "Green":
+    print("You've just earned 5 points ")
+#second version
+alien_color:str = "Yellow"
+if alien_color == "Green":
+    print("You've just earned 5 points ")
+
+'''
+5-4. Alien Colors #2: Choose a color for an alien as you did in Exercise 5-3, and write an if-else chain.
+• If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+• If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+• Write one version of this program that runs the if block and another that runs the else block.
+'''
+alien_color:str = "Green"
+if alien_color == "Green":
+    print("You've just earned 5 points for shooting the alien ")
+else:
+    print("You've just earned 10 points")
+#second version
+alien_color:str = "Red"
+if alien_color == "Green":
+    print("You've just earned 5 points for shooting the alien ")
+else:
+    print("You've just earned 10 points")
+
+'''
+5-5. Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-elif-else chain.
+
+• If the alien is green, print a message that the player earned 5 points.
+• If the alien is yellow, print a message that the player earned 10 points.
+• If the alien is red, print a message that the player earned 15 points.
+• Write three versions of this program, making sure each message is printed for the appropriate color alien.
+'''
+alien_color:str = "Green"
+if alien_color == "Green":
+    print("You've just earned 5 points")
+elif alien_color == "Yellow":
+    print("You've just earned 10 points")
+else:
+    print("You've just earned 15 points")
+#second version
+alien_color:str = "Yellow"
+if alien_color == "Green":
+    print("You've just earned 5 points")
+elif alien_color == "Yellow":
+    print("You've just earned 10 points")
+else:
+    print("You've just earned 15 points")
+#third version
+alien_color:str = "Red"
+if alien_color == "Green":
+    print("You've just earned 5 points")
+elif alien_color == "Yellow":
+    print("You've just earned 10 points")
+else:
+    print("You've just earned 15 points")
+
+'''
+5-6. Stages of Life: Write an if-elif-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
+• If the person is less than 2 years old, print a message that the person is a baby.
+• If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+• If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+• If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+• If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+• If the person is age 65 or older, print a message that the person is an elder.
+'''
+age:int = 18
+if age < 2:
+    print("This person is a baby")
+elif age >= 2 and age < 4:
+    print("This person is a toddler")
+elif age >= 4 and age < 13:
+    print("This person is a kid")
+elif age >= 13 and age < 20:
+    print("This person is a teeneger")
+elif age >= 20 and age < 65:
+    print("This person is an adult ")
+else:
+    print("This person is an elder")
+
+'''
+5-7. Favorite Fruit: Make a list of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your list.
+• Make a list of your three favorite fruits and call it favorite_fruits.
+• Write five if statements. Each should check whether a certain kind of fruit is in your list. If the fruit is in your list, the if block should print a statement, such as You really like Apples!
+'''
+favorite_fruits:list = ["Apple", "Banana", "Strawberry", "Watermelon", "Grape"]
+if "Apple" in favorite_fruits:
+    print("I love Apples")
+if "Banana" in favorite_fruits:
+    print("Banana is my favorite fruit")
+if "Strawberry" in favorite_fruits:
+    print("I like strawberries")
+if "Watermelom" in favorite_fruits:
+    print("Watermelon is so fresh")
+if "Grape" in favorite_fruits:
+    print("Grape is very good")
+
+'''
+5-8. Hello Admin: Make a list of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the list, and print a greeting to each user.
+• If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+• Otherwise, print a generic greeting, such as Hello Jaden, thank you for logging in again.
+'''
+usernames:list = ["mario", "carlo", "flavio", "andrea", "marta", "admin"]
+for username in usernames:
+    if username == "admin":
+        print("Hello admin, would you like to see a status report?")
+    else:
+        print(f"Hello {username}, thank you for logging in again ")
+
+'''
+5-9. No Users: Add an if test to hello_admin.py to make sure the list of users is not empty.
+• If the list is empty, print the message We need to find some users!
+• Remove all of the usernames from your list, and make sure the correct message is printed.
+'''
+usernames:list = ["mario", "carlo", "flavio", "andrea", "marta", "admin"]
+usernames.clear()
+if not usernames:
+    print("We need to find some users!")
+
+'''
+5-10. Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+• Make a list of five or more usernames called current_users.
+• Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
+• Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available.
+• Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted. (To do this, you’ll need to make a copy of current_users containing the lowercase versions of all existing users.)
+'''
+current_users:list = ["mario", "carlo", "flavio", "andrea", "marta", "admin"]
+new_users:list = ["sara", "marco", "simone", "gaia", "lorenzo", "admin"]
+for user in new_users:
+    if user in current_users:
+        print(f"{user} it has been used, please enter a new username")
+    else:
+        print(f"the username {user} is available")
+
+'''
+5-11. Ordinal Numbers: Ordinal numbers indicate their position in a list, such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
+• Store the numbers 1 through 9 in a list.
+• Loop through the list.
+• Use an if-elif-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line.
+'''
+ordinal_numbers:list = list(range(1,10))
+for num in ordinal_numbers:
+    if num is 1:
+        print(f"{num}st")
+    elif num is 2:
+        print(f"{num}nd")
+    elif num is 3:
+        print(f"{num}rd")
+    else:
+        print(f"{num}th")
+    
