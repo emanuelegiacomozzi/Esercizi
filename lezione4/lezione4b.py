412aef678e0a1b5e9cca2df4ea9dcf1c0f900d00
emanuelegiacomozzi <emanuelegiacomozzi2018@gmail.com>
2024-04-28 11:58:45 +0200

    esercizio 14 finito

--- lezione4/lezione4b.py
+++ lezione4/lezione4b.py
@@ -66,6 +66,17 @@ print(make_album("Ed Sheraan", "Divide", songs=14))
 '''
 8-8. User Albums: Start with your program from Exercise 8-7. Write a while loop that allows users to enter an album’s artist and title. Once you have that information, call make_album() with the user’s input and print the dictionary that’s created. Be sure to include a quit value in the while loop.
 '''
+def make_album(artist:str, album:str,):
+   artist_album:dict = {"artist": {artist}, "album": {album}}
+   return artist_album
+while True:
+   artist:str = input("Artist: ")
+   if artist == "quit":
+      break
+   album:str = input("Album: ")
+   if album == "quit":
+      break
+   print(make_album(artist,album))
 
 
 '''
