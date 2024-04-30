b674f5b4aa218d28e986be890bd8bb0c12a7dc47
emanuelegiacomozzi <emanuelegiacomozzi2018@gmail.com>
2024-04-30 12:53:59 +0200

    esercizi 15 e 16 obbligatori con import

--- lezione4/printing_functions.py
+++ lezione4/printing_functions.py
@@ -1,5 +1,5 @@
 '''
-8-14. Cars: Write a function that stores information about a car in a dictionary. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Your function should work for a call like this one: car = make_car('subaru', 'outback', color='blue', tow_package=True) Print the dictionary that’s returned to make sure all the information was stored correctly. 
+8-15. Printing Models: Put the functions for the example printing_models.py in a separate file called printing_functions.py. Write an import statement at the top of printing_models.py, and modify the file to use the imported functions.
 '''
 from lezione4b import make_cars
 
