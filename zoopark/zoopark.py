d3326d99590e2447d68f8305ebfad5877a13db6f
emanuelegiacomozzi <167187063+emanuelegiacomozzi@users.noreply.github.com>
2024-05-08 19:13:27 +0200

    esercizi zoopark

---
+++ zoopark/zoopark.py
@@ -0,0 +1,20 @@
+class Zoo:
+    def __init__(self,fences:str,zoo_keepers:str):
+        self.fences = fences
+        self.zoo_keepers = zoo_keepers
+
+class Animal:
+    def __init__(self, name:str, species:str, age:int, height:float, width:float, preferred_habitat:str, health:float):
+        self.name = name
+        self.species = species
+        self.age = age
+        self.height = height
+        self.width = width
+        self.preferred_habitat = preferred_habitat
+        self.health = health
+
+class Fence:
+    def __init__(self,area:float, temperature:float, habitat:str):
+        self.area = area
+        self.temperature = temperature
+        self.habitat = habitat
