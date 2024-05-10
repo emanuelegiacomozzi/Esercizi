4393aef15aedbcf15c6af250f5da22c74cf51533
emanuelegiacomozzi <emanuelegiacomozzi2018@gmail.com>
2024-05-10 19:27:29 +0200

    esercizi riepilogativi sulle funzioni

--- lezione7/lezione7.py
+++ lezione7/lezione7.py
@@ -38,3 +38,37 @@ def filtra_e_mappa(prodotti: dict[str:float]) -> list[str:float]:
             return prodotti_scontati
 print(filtra_e_mappa({'Penna': 15.0, 'Zaino': 50.0, 'Quaderno': 22.0}))
 print(filtra_e_mappa({'Tavolo': 120.0, 'Sedia': 85.0}))
+
+#esercizio 5
+
+def create_contact(name:str, email:str=None, telefono:int=None):
+    contact = {"profile":name, "email":email, "telefono":telefono}
+    return contact
+contact1 = create_contact("Emanuele Giacomozzi", "emanuele@gmail.com", 33344455567)
+print(contact1)
+
+def update_contact(contact:dict, name:str, email:str=None, telefono:int=None):
+    if contact["profile"] == name:
+        if email:
+            contact["email"] = email
+        if telefono:
+            contact["telefono"] = telefono
+
+    return contact
+contact1 = create_contact("Emanuele Giacomozzi", "emanuele@gmail.com", 3335554466)
+print(contact1)
+
+#esercizio 6
+
+def aggrega_voti(registro:list[dict]):
+    new_registro = {}
+    for studente in registro:
+        nome = studente["name"]
+        voto = studente["voto"]
+
+        if nome in new_registro:
+            new_registro[nome].append(voto)
+        else:
+            new_registro[nome] = [voto]
+    return new_registro
+print(aggrega_voti([{"name": "Alice", "voto":6}, {"name":"Bob","voto":7},{}]))
