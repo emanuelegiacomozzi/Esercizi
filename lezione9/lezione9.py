6af5e011d39cd6d04c081ceeb154f2db32cf59e8
emanuelegiacomozzi <emanuelegiacomozzi2018@gmail.com>
2024-05-15 11:40:32 +0200

    booble sort

--- lezione9/lezione9.py
+++ lezione9/lezione9.py
@@ -1,10 +1,16 @@
 def bubble_sort(x:list[float]):
-    for _ in range(len(x)):
-        for j in range(len(x) - 1):
+    for i in range(len(x)):
+        swap:bool = False
+        for j in range(len(x) - i - 1):
             if x[j] > x[j+1]:
                 temp:float = x[j+1]
+                swap = True
                 x[j+1] = x[j]
                 x[j] = temp
-a = [8,2,3,7,5]
-bubble_sort(a)
+        if not swap:
+            break
+
+a = [1,3,2,4,5]
+swap = bubble_sort(a)
+print(f"Ho fatto degli swap = {swap}")
 print(a)
