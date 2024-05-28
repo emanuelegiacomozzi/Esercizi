9a1b63a41744eec2a40ee0136bd20230e715c4fe
emanuelegiacomozzi <emanuelegiacomozzi2018@gmail.com>
2024-05-28 19:03:45 +0200

    esercizio

--- lezione7/lepre_tartaruga.py
+++ lezione7/lepre_tartaruga.py
@@ -1,53 +1,59 @@
 import random
 
-def posizioni(percorso, posizione_t, posizione_h):
-    percorso[0] = posizione_t
-    percorso[0] = posizione_h
-    return percorso
-percorso = ['_']*70
-posizione_t = 'T'
-posizione_h = 'H'
-print(posizioni(percorso, posizione_t, posizione_h))
+def posizioni(posizione_t, posizione_h):
+    percorso = ['_']*70
+    if posizione_t == posizione_h:
+        percorso[posizione_t - 1] = 'OUCH!!!'
+    else:
+        percorso[posizione_t - 1] = 'T'
+        percorso[posizione_h - 1] = 'H'
+    print(''.join(percorso))
 
-def mosse_tartaruga(posizione_t):
+def mosse_tartaruga(tartaruga,meteo):
     i:int = random.randint(1,10)
     if 1 <= i <= 5:
-        posizione_t += 3
+        tartaruga += 3
     elif 6 <= i <= 7:
-        posizione_t -= 6
+        tartaruga -= 6
     elif i < 1:
-        posizione_t = 1
+        tartaruga = 1
     else:
-        posizione_t += 1
-    return posizione_t
+        tartaruga += 1
+    if meteo == "piogga":
+        tartaruga -= 2
+    return tartaruga
 
 
 
-def mosse_lepre(posizione_h):
+def mosse_lepre(lepre,meteo):
     i:int = random.randint(1,10)
-    posizione_h = 0
+    lepre = 0
     if 1 <= i <= 2:
-        posizione_h = 0
+        lepre = 0
     elif 3 <= i <= 4:
-        posizione_h += 9
-    elif i <= 5:
-        posizione_h -= 12
+        lepre += 9
+    elif i == 5:
+        lepre -= 12
     elif 6 <= i <= 8:
-        posizione_h += 1
+        lepre += 1
     elif 9 <= i <= 10:
-        posizione_h -= 2
+        lepre -= 2
     elif i < 1:
-        posizione_h = 1
-    return posizione_h
+        lepre = 1
+    if meteo == "piogga":
+        lepre -= 2
+    return lepre
 
 
-def gara():
+def  gara():
     posizione_t = 1
     posizione_h = 1
+    meteo = "soleggiato"
     print("'BANG !!!!! AND THEY'RE OFF !!!!!'")
     while True:
-            posizione_t = mosse_tartaruga(posizione_t)
-            posizione_h = mosse_lepre(posizione_h)
+            posizione_t = mosse_tartaruga(posizione_t, meteo)
+            posizione_h = mosse_lepre(posizione_h, meteo)
+            posizioni(mosse_tartaruga, mosse_lepre)
             if posizione_t >= 70:
                 print("TORTOISE WINS! || VAY!!!")
                 break
@@ -57,6 +63,7 @@ def gara():
             elif posizione_h >= 70 and posizione_t >= 70:
                 print("IT'S A TIE.")
                 break
+gara()
 
 
 
