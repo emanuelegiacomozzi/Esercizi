36b9c1de1d592153b637cfa8630e650cf82f0572
emanuelegiacomozzi <emanuelegiacomozzi2018@gmail.com>
2024-06-02 11:16:20 +0200

    Libreria e Catalogo Film

--- lezione12/esercitazione_classi.py
+++ lezione12/esercitazione_classi.py
@@ -74,3 +74,69 @@ print(biblioteca.presta_libro("Harry Potter"))
 print(biblioteca.restituisci_libro("Mia"))
 
 print(biblioteca.mostra_libri())
+
+
+#Catalogo Film
+
+class MovieCatalog:
+
+    def __init__(self):
+
+        self.catalogo:dict[str,list[str]] = {}
+        
+    
+    def add_movie(self, director_name:str, movies:list):
+        if director_name in self.catalogo:
+            self.catalogo[director_name].extend(movies)
+        else:
+            self.catalogo[director_name] = movies
+    
+    def remove_movie(self, director_movie:str, movie_name:str):
+        if director_movie in self.catalogo:
+            if movie_name in self.catalogo[director_movie]:
+                self.catalogo[director_movie].remove(movie_name)
+            elif not self.catalogo[director_movie]:
+                del self.catalogo[director_movie] 
+        return f"{movie_name} eliminato dal catalogo"
+    
+    def list_directors(self):
+        return self.catalogo.keys()
+    
+    def get_movies_by_director(self, director_name:str):
+        if director_name in self.catalogo:
+            return self.catalogo[director_name]
+        else:
+            return f"{director_name} non presente nel catalogo"
+
+    def search_movies_by_title(self, title:str):
+        result = {}
+        for director_name,movies in self.catalogo.items():
+            matching_movies = []
+            for movie in movies:
+                if title.lower() in movie.lower():
+                    matching_movies.append(movie)
+            if matching_movies:
+                    result[director_name] = matching_movies
+        if result:
+            return result
+        
+    def catalogo_completo(self):
+        return self.catalogo
+        
+            
+catalogo = MovieCatalog()
+
+catalogo.add_movie("Nolan", ["Inception"])
+catalogo.add_movie("Nolan", ["Memento", "The Prestige"])
+catalogo.add_movie("Tarantino", ["Il Padrino |", "Il Padrino ||"])
+
+print(catalogo.remove_movie("Nolan","Inception"))
+
+print("Catalogo: " , catalogo.catalogo_completo())
+
+print("Registi: ", catalogo.list_directors())
+
+print(catalogo.get_movies_by_director("Tarantino"))
+
+print(catalogo.search_movies_by_title("|"))
+
