1c483e0d492a81d20a0a1833af36900a3dcb0b6c
emanuelegiacomozzi <emanuelegiacomozzi2018@gmail.com>
2024-06-17 10:55:25 +0200

    dottore

--- lezione17/sistema_ospedaliero/dottore.py
+++ lezione17/sistema_ospedaliero/dottore.py
@@ -2,10 +2,9 @@ from persona import Persona
 
 class Dottore(Persona):
 
-    def __init__(self, first_name: str, last_name: str, age:int, specialization:str, parcel:float):
+    def __init__(self, first_name: str, last_name: str,specialization:str, parcel:float):
         super().__init__(first_name, last_name)
 
-        self.age = age
         self.specialization = specialization
         self.parcel = parcel
 
