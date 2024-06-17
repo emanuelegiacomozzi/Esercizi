ccf069afab41a0fadffd8ed2ede15be7cca8d751
emanuelegiacomozzi <emanuelegiacomozzi2018@gmail.com>
2024-06-17 10:59:00 +0200

    paziente

--- lezione17/sistema_ospedaliero/paziente.py
+++ lezione17/sistema_ospedaliero/paziente.py
@@ -2,10 +2,9 @@ from persona import Persona
 
 class Paziente(Persona):
 
-    def __init__(self, first_name: str, last_name: str, age:int, idCode:str):
+    def __init__(self, first_name: str, last_name: str, idCode:str):
         super().__init__(first_name, last_name)
-    
-        self.age = age  
+     
         self.idCode = idCode
     
     def setIdCode(self, idCode:str):
@@ -20,7 +19,3 @@ class Paziente(Persona):
 
         print(f"Paziente: {self.first_name} {self.last_name}\nID: {self.idCode}")
 
-paziente = Paziente("Mattia", "Neri", 34, "Hb34")
-paziente.setIdCode("Hnbcd")
-print(paziente.getidCode())
-paziente.patientInfo()
