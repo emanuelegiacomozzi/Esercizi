1dd04faa9bb34fa12d597410f165b2e47b98f1e2
emanuelegiacomozzi <emanuelegiacomozzi2018@gmail.com>
2024-06-19 12:19:32 +0200

    movie_genre

--- lezione17/blockbuster/movie_genre.py
+++ lezione17/blockbuster/movie_genre.py
@@ -2,7 +2,7 @@ from blockbuster.film import Film
 
 class Azione(Film):
 
-    def __init__(self, id: int, title: str, genere:str, penale:float):
+    def __init__(self, id: int, title: str):
         super().__init__(id, title)
         genere = "Azione"
         penale = 3
