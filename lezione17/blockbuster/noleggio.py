f0b9321727a46c16393faf87a2206f51ab31d23d
emanuelegiacomozzi <emanuelegiacomozzi2018@gmail.com>
2024-06-21 11:40:05 +0200

    noleggio

--- lezione17/blockbuster/noleggio.py
+++ lezione17/blockbuster/noleggio.py
@@ -6,14 +6,45 @@ class Noleggio:
 
         self.film_list = film_list
         self.rented_film:dict = {}
+        self.film_noleggiati = []
     
     def isAvaible(self, film:str):
 
-        titolo_film = film
         if film not in self.film_list:
-            print(f"Il film scelto non è disponibile: {titolo_film}! ")
+            print(f"Il film scelto non è disponibile: {film}! ")
             return False
         if film in self.film_list:
-            print(f"Il film scelto è disponibile: {titolo_film}!")
+            print(f"Il film scelto è disponibile: {film}!")
             return True
-               
+        
+    def rentAMovie(self, film:str, clientID:str):
+
+        if film in self.film_list and self.isAvaible(film):
+            self.film_list.remove(film)
+            if clientID in self.rented_film:
+                self.rented_film[clientID].append(film)
+            else:
+                self.rented_film[clientID] = [film]
+            print(f"Il cliente {clientID} ha noleggiato {film}!")
+        else:
+            print(f"Non è possibile noleggiare il film {film}!")
+    
+    def giveBack(self, film:str, clientID:str, days:int):
+
+        if clientID in self.rented_film and film in self.rented_film[clientID]:
+            self.rented_film[clientID].remove(film)
+            self.film_list.append(film)
+            penale = film.calcolaPenaleRitardo(days)
+            print(f"Cliente: {clientID}! La penale da pagare per il film {film} è di {penale} euro!")
+    
+    def printMovies(self):
+
+        for film in self.film_list:
+
+            print(f"{film}-{film.getGenere()}-")
+
+    def printRentMovies(self, clientID):
+
+        for film in self.rented_film[clientID]:
+
+            print(film)
