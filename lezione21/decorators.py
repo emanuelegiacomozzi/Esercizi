5c696f8054d55f7d6bf38d663423d7c35cf94194
emanuelegiacomozzi <emanuelegiacomozzi2018@gmail.com>
2024-06-27 13:34:37 +0200

    decorator

--- lezione21/decorators.py
+++ lezione21/decorators.py
@@ -107,3 +107,38 @@ def area_cerchio(raggio:float):
 area_cerchio(5)
 
 
+################################################
+def generatore():
+
+    yield "A"
+    yield "B"
+    yield "C"
+
+prova_generatore = generatore()
+
+print(next(prova_generatore))
+print(next(prova_generatore))
+print(next(prova_generatore))
+
+################################################
+
+from contextlib import contextmanager
+
+@contextmanager
+def context_manager_decorator(*args):
+    import time
+
+    start_time: float = time.time()
+
+    yield
+
+    end_time: float = time.time()
+    elapsed_time : float = end_time - start_time
+    print(f"{elapsed_time=}")
+
+@context_manager_decorator
+def area_cerchio(raggio:float):
+
+    return raggio * raggio * 3.14
+
+area_cerchio(1)
